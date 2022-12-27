9d0a4fe79938d22727620df5740d3cea4df94c4e
Luiz Fernando <luizfernando.dev06@gmail.com>
2022-12-27 10:00:24 -0300

    commit

---
+++ JavaScript/fundamentosJS/tipagem.js
@@ -0,0 +1,9 @@
+//JavaScript tem sua tipagem fraca, isto é, a mesma variável pode receber valores diferentes 
+
+let qualquer = 'Texto'
+console.log(qualquer)
+console.log(typeof(qualquer))
+
+qualquer = 3.5478
+console.log(qualquer)
+console.log(typeof(qualquer))
