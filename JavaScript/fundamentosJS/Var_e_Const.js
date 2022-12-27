3b36c42638d62fb3c320275c85e0e1c6036ac171
Luiz Fernando <luizfernando.dev06@gmail.com>
2022-12-27 09:43:40 -0300

    commit

---
+++ JavaScript/fundamentosJS/Var_e_Const.js
@@ -0,0 +1,8 @@
+var a = 3
+let b = 4
+/* Observe que as duas maneiras de se declarar uma variável está correta 
+    porém, declarando da maneira var, podemos criar diversas variáveis
+    com o mesmo nome, já em let, apenas aquela variável terá aquele nome.*/
+const c = 37
+
+// Da maneira cosnt, a variável c não pode mudar seu valor
---
+++ JavaScript/fundamentosJS/comentarios.js
@@ -0,0 +1,9 @@
+//Comentario de uma linha 
+console.log('Linha 1')
+
+/*
+
+Multiplas linhas  
+
+*/
+console.log('Linha 2')
