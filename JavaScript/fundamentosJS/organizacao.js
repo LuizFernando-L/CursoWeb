3e4c9cc54fbf6b25eb89aa207d9884f503d5fe47
Luiz Fernando <luizfernando.dev06@gmail.com>
2022-12-27 09:04:42 -0300

    commit

---
+++ JavaScript/fundamentosJS/organizacao.js
@@ -0,0 +1,8 @@
+console.log("Sentença de código")
+/* JavaScript é organizado em sentenças de códigos e blocos que podem ou não terminar em ; */
+{
+    {
+        console.log("Olá");
+        console.log('Mundo!')
+    }
+}
