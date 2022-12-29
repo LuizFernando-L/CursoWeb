55b2610df612f338873acbc329994b3719f9cd8a
Luiz Fernando <luizfernando.dev06@gmail.com>
2022-12-28 21:47:38 -0300

    commit

--- JavaScript/fundamentosJS/numeros.js
+++ JavaScript/fundamentosJS/numeros.js
@@ -2,4 +2,12 @@ const peso1 = 1.0
 const peso2 = Number('2.0')
 
 console.log(peso1, peso2)
-console.logo(Number.isInteger(peso1))
+console.log(Number.isInteger(peso1))
+console.log(Number.isInteger(peso2))
+
+const avaliacao1 = 9.871
+const avaliacao2 = 6.874
+
+const total = (avaliacao1*peso1 + avaliacao2*peso2)/2
+
+console.log(total.toFixed(2))
