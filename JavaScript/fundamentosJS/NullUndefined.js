28fa4b9e57728fdac5ed29e0ee3a4b3ebce9ce0c
Luiz Fernando <luizfernando.dev06@gmail.com>
2023-01-03 09:52:29 -0300

    commit

---
+++ JavaScript/fundamentosJS/NullUndefined.js
@@ -0,0 +1,5 @@
+let valor // não inicializada 
+console.log(valor) // valor undefined pois está variável aponta para um endereço de memória 
+
+valor = null //ausência de valor 
+console.log(valor)// valor nulo pois a variável foi inicializada com uma ausência de valor
