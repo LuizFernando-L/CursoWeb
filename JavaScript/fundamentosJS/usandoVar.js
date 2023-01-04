10d55977f04a3c9fb11ecd8603eb180c67a14f7b
Luiz Fernando <luizfernando.dev06@gmail.com>
2023-01-04 11:23:35 -0300

    commit

---
+++ JavaScript/fundamentosJS/usandoVar.js
@@ -0,0 +1,6 @@
+//Uma variável normalmente se criada dentro de um bloco de código, ela fica visível somente no bloco, o que não é o caso de variável declarada como var
+{
+    var sera = 'Sera?'
+    console.log(sera)
+}
+console.log(sera)
